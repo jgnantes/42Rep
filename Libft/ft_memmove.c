0406eefa0c26301a8e6286fc9b19327e61c4859e
jgnantes <jgnantes@outlook.com>
2025-10-21 19:36:02 -0300

    v1

--- Libft/ft_memmove.c
+++ Libft/ft_memmove.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//TÁ ERRADO, TEM QUE FAZER SEM BUFFER TEMPORÁRIO
 void    *ft_memmove(void *dest, const void *src, size_t n)
 {
         size_t  i;
