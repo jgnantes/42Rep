f75a3953bb0d5459023fffb4b8d4262ac1dde824
jgnantes <jgnantes@outlook.com>
2025-10-22 19:27:38 -0300

    v1

--- Libft/ft_strchr.c
+++ Libft/ft_strchr.c
@@ -17,11 +17,13 @@ char	*ft_strchr(const char *s, int c)
 	int	i;
 	
 	i = 0;
-	while (s[i] != (char)c)
+	while (s[i] != '\0')
 	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
 		i++;
-		if (s[i] == '\0' && c != 0)
-			return (NULL);
 	}
-	return ((char *)&s[i]);
+	if (c == 0)
+		return((char *)&s[i]);
+	return (NULL);
 }
--- Libft/ft_strrchr.c
+++ Libft/ft_strrchr.c
@@ -12,18 +12,16 @@
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+char    *ft_strrchr(const char *s, int c)
 {
-	int	i;
-	
-	if (s[0] == '\0')
-		return ((char *)&s[0]);
-	i = ft_strlen(s);
-	while (s[i] != (char)c)
-	{
-		i--;
-		if (s[i] == '\0' && c != 0)
-			return (NULL);
-	}
-	return ((char *)&s[i]);
+    int i;
+
+    i = ft_strlen(s);
+    while (i >= 0)
+    {
+        if (s[i] == (char)c)
+            return ((char *)&s[i]);
+        i--;
+    }
+    return (NULL);
 }
